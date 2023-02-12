6288d91bdff4a247357b5a027adb70a16423b1b0
Aleksandr Gorodnichev <limedigital@yandex.ru>
2023-02-12 21:43:05 +0300

    15.1.5

--- backend/app.js
+++ backend/app.js
@@ -4,27 +4,40 @@ const { celebrate, Joi, errors } = require('celebrate');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
-const cors = require('cors');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const linkCheck = require('./constants/constant');
+const { linkCheck, allowedCors } = require('./constants/constant');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const {
-  allowOrigin, allowOptions,
-} = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors());
-
 app.use(express.json());
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+  }
+
+  return next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -52,9 +65,6 @@ app.post('/signin', celebrate({
 app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-
-app.use(allowOrigin);
-app.use(allowOptions);
 
 app.use(errorLogger);
 
