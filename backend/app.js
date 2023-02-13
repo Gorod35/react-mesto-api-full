4a06b6a5ef10dec808aa533bdb421141b6fd72a1
Aleksandr Gorodnichev <limedigital@yandex.ru>
2023-02-13 21:57:28 +0300

    15.1.8

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,6 @@ const auth = require('./middlewares/auth');
 const { linkCheck, allowedCors } = require('./constants/constant');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,7 +20,24 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 app.use(requestLogger);
 
-app.use(cors());
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    return res.end();
+  }
+
+  return next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
